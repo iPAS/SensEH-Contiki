5494c599f891fbe17452264a449311dfd8e612f1
ipas <ptiwatthanont@gmail.com>
2015-12-03 08:00:55 +0700

    Detect depleting node, and report.

--- tools/cooja/apps/senseh/java/EHNode.java
+++ tools/cooja/apps/senseh/java/EHNode.java
@@ -18,68 +18,84 @@ import se.sics.cooja.mspmote.SkyMote;
 @ClassDescription("A node with Energy Harvesting")
 public class EHNode{
 
-  private static Logger logger = Logger.getLogger(EHNode.class);
+    private static Logger logger = Logger.getLogger(EHNode.class);
 
-  private Simulation simulation;
-  private int nodeID;
-  private Mote mote;
-  private EHSystem ehSys;
-  private Pin storageMotePin;
-  private PowerConsumption consumption;
+    private Simulation simulation;
+    private int nodeID;
+    private Mote mote;
+    private EHSystem ehSys;
+    private Pin storageMotePin;
+    private PowerConsumption consumption;
 
-  private double lastEnergyConsumed;
-  private double lastTotalEnergyConsumed;
+    private double lastEnergyConsumed;
+    private double lastTotalEnergyConsumed;
 
-  private String configFilePath;
-
-
-  public EHSystem getEHSystem() {
-    return ehSys;
-  }
-
-  public PowerConsumption getPowerConsumption() {
-    return consumption;
-  }
-
-  public int getNodeID() {
-    return nodeID;
-  }
-
-  public double getLastEnergyConsumed() {
-    return lastEnergyConsumed;
-  }
-
-  public double getLastTotalEnergyConsumed() {
-    return lastTotalEnergyConsumed;
-  }
+    private String configFilePath;
+    private SensEHGUI senseh;
 
 
-  public EHNode(int nodeID, Simulation simulation, String configFilePath) {
-    this.nodeID = nodeID + 1;
-    this.simulation = simulation;
-    mote = simulation.getMote(nodeID);
-    this.configFilePath = configFilePath;
+    public EHSystem getEHSystem() {
+        return ehSys;
+    }
 
-    ehSys = new EHSystem(this.configFilePath, this.nodeID);
-    storageMotePin = new Pin(ehSys.getStorage(), (SkyMote)mote);
-    consumption = new PowerConsumption(simulation, mote, ehSys.getVoltage());
-  }
+    public PowerConsumption getPowerConsumption() {
+        return consumption;
+    }
 
-  public void updateCharge(){ // iPAS: the EH system model of the node
-    chargeStorage();
-    dischargeConsumption();
-    consumption.setVoltage(ehSys.getVoltage()); // Assume that it's fixed, and regulated.
-  }
+    public int getNodeID() {
+        return nodeID;
+    }
 
-  private void chargeStorage(){
-    ehSys.harvestCharge();
-  }
+    public double getLastEnergyConsumed() {
+        return lastEnergyConsumed;
+    }
 
-  private void dischargeConsumption(){
-    double energyConsumed = ehSys.getChargeInterval() /*sec*/ * consumption.getAveragePower() /*mW*/;
-    consumption.snapStatistics(); // Snap the consumed energy at the time
-    ehSys.consumeCharge(energyConsumed);
-    consumption.reset();
-  }
+    public double getLastTotalEnergyConsumed() {
+        return lastTotalEnergyConsumed;
+    }
+
+    public EHNode(int nodeID, Simulation simulation, String configFilePath, SensEHGUI senseh) {
+        this.nodeID = nodeID + 1;
+        this.simulation = simulation;
+        mote = simulation.getMote(nodeID);
+        this.configFilePath = configFilePath;
+        this.senseh = senseh;
+
+        ehSys = new EHSystem(this.configFilePath, this.nodeID);
+        storageMotePin = new Pin(ehSys.getStorage(), (SkyMote)mote);
+        consumption = new PowerConsumption(simulation, mote, ehSys.getVoltage());
+    }
+
+    public void updateCharge(){  // [iPAS]: the EH system model of the node
+        chargeStorage();
+        dischargeConsumption();
+        consumption.setVoltage(ehSys.getVoltage());  // Assume that it's fixed, and regulated.
+
+        // TODO [iPAS]: mote.get ... stop mote if drained out
+        if (((Battery)ehSys.getStorage()).isDepleted()) {
+            //((SkyMote)mote).getCPU().stop()
+            //((SkyMote)mote).getCPU().isRunning()
+            //((SkyMote)mote).getCPU().reset();
+
+            if (!SensEHGUI.QUIET) {
+                String str = "Mote #" + mote.getID() + "'s battery is empty at (ms): "
+                        + simulation.getSimulationTimeMillis();
+                this.senseh.log.addMessage(str);
+                logger.info(str);
+            }
+        }
+    }
+
+    private void chargeStorage(){
+        ehSys.harvestCharge();
+    }
+
+    private void dischargeConsumption(){
+        double energyConsumed = ehSys.getChargeInterval()  /*sec*/
+                              * consumption.getAveragePower()  /*mW*/;
+        consumption.snapStatistics();  // Snap the consumed energy at the time
+        ehSys.consumeCharge(energyConsumed);
+        consumption.reset();
+    }
 
 }
--- tools/cooja/apps/senseh/java/SensEHGUI.java
+++ tools/cooja/apps/senseh/java/SensEHGUI.java
@@ -34,252 +34,253 @@ import java.util.Collection;
 @ClassDescription("SensEH GUI")
 @PluginType(PluginType.SIM_PLUGIN)
 public class SensEHGUI extends VisPlugin {
-  private static Logger logger = Logger.getLogger(SensEHGUI.class);
+    private static Logger logger = Logger.getLogger(SensEHGUI.class);
 
-  private Simulation simulation;
-  private EHNode [] ehNodes;
+    private Simulation simulation;
+    private EHNode [] ehNodes;
 
-  private long startTime; // uS
-  private long lastUpdateTime; // uS
-  private ChargeUpdateEvent chargeUpdateEvent;
-  private long totalUpdates;
+    private long startTime; // uS
+    private long lastUpdateTime; // uS
+    private ChargeUpdateEvent chargeUpdateEvent;
+    private long totalUpdates;
 
-  private File ehConfigFile = null;
+    private File ehConfigFile = null;
 
-  private MessageList log = new MessageList();
-  private static final boolean QUIET = false;
+    public MessageList log = new MessageList();  // [iPAS]: Changing to public for accessing by EHNode
+    public static final boolean QUIET = false;  // [iPAS]: Changing to public for accessing by EHNode
 
 
-  public SensEHGUI(Simulation simulation, final GUI gui) {
-    super("SensEH Plugin", gui, false);
-    this.simulation = simulation;
-    //consumption = new Consumption(simulation);
+    public SensEHGUI(Simulation simulation, final GUI gui) {
+        super("SensEH Plugin", gui, false);
+        this.simulation = simulation;
+        //consumption = new Consumption(simulation);
 
-    log.addPopupMenuItem(null, true); /* Create message list popup */
-    add(new JScrollPane(log));
+        log.addPopupMenuItem(null, true); /* Create message list popup */
+        add(new JScrollPane(log));
 
-    if (!QUIET) {
-      log.addMessage("Harvesting plugin started at (ms): " + simulation.getSimulationTimeMillis());
-      logger.info("Harvesting plugin started at (ms): " + simulation.getSimulationTimeMillis());
-    }
-    setSize(500, 200);
-  }
-
-  @Override
-  public void startPlugin() {
-    super.startPlugin();
-
-    if (ehConfigFile != null)
-      return;
-
-    JFileChooser fileChooser = new JFileChooser();
-    File suggest = new File(GUI.getExternalToolsSetting("DEFAULT_EH_CONFIG",
-        "/home/user/contiki-2.7/tools/cooja/apps/senseh/config/EH.config"));
-    fileChooser.setSelectedFile(suggest);
-    fileChooser.setDialogTitle("Select configuration file for harvesting system");
-    int reply = fileChooser.showOpenDialog(GUI.getTopParentContainer());
-    if (reply == JFileChooser.APPROVE_OPTION) {
-      ehConfigFile = fileChooser.getSelectedFile();
-      GUI.setExternalToolsSetting("DEFAULT_EH_CONFIG", ehConfigFile.getAbsolutePath());
-    }
-    if (ehConfigFile == null) {
-      throw new RuntimeException("No configuration file for harvesting system");
-    }
-    init(ehConfigFile.getAbsolutePath());
-  }
-
-  void init(String configFilePath) {
-    //setTitle("~~~ TITLE ~~~");
-    ehNodes = new EHNode[simulation.getMotesCount()];
-    for (int i = 0; i < simulation.getMotesCount(); i++)
-      ehNodes[i] = new EHNode(i, simulation, configFilePath);
-    schedulePeriodicChargeUpdate(); // schedule event to update the charge of all the nodes
-  }
-
-  private void schedulePeriodicChargeUpdate() {
-    simulation.invokeSimulationThread(new ChargeUpdateTaskScheduler());
-  }
-
-  private double getChargeInterval() { // assume that charge update interval for ALL nodes is equal
-    return ehNodes[0].getEHSystem().getChargeInterval();
-  }
-
-  @Override
-  public void closePlugin() {
-    if (chargeUpdateEvent != null) {
-      chargeUpdateEvent.remove();
-
-      for (int i = 0; i < ehNodes.length; i++) {
-        ehNodes[i].getPowerConsumption().dispose();
-      }
-    }
-  }
-
-  // --------------------------------------------------------------------------
-  private class ChargeUpdateTaskScheduler implements Runnable {
-
-    @Override
-    public void run() {
-      totalUpdates = 1;
-      startTime = simulation.getSimulationTime();
-      lastUpdateTime = 0; // It means never updated before.
-      //logger.debug("periodStart: " + periodStart);
-      chargeUpdateEvent = new ChargeUpdateEvent(0);
-      chargeUpdateEvent.execute(startTime + (long)(getChargeInterval() * 1000000));
-    }
-
-  }
-
-  // --------------------------------------------------------------------------
-  private class ChargeUpdateEvent extends TimeEvent{
-
-    public ChargeUpdateEvent(long t) {
-      super(t, "charge update event");
+        if (!QUIET) {
+            log.addMessage("Harvesting plugin started at (ms): " + simulation.getSimulationTimeMillis());
+            logger.info("Harvesting plugin started at (ms): " + simulation.getSimulationTimeMillis());
+        }
+        setSize(500, 200);
     }
 
     @Override
-    public void execute(long t) {
-      // Detect early events: reschedule for later
-      //System.out.println ("t\t"+t + "\tSimTime\t"+simulation.getSimulationTime());
-      if (simulation.getSimulationTime() < t) {
-        simulation.scheduleEvent(this, startTime + (long)(totalUpdates * getChargeInterval() * 1000000));
-        return;
-      }
+    public void startPlugin() {
+        super.startPlugin();
 
-      lastUpdateTime = simulation.getSimulationTime();
-      /* SensEH does NOT continuously count harvested energies and consumed energies.
-       * It updates the charges periodically on every interval.
-       */
-      for (EHNode node : ehNodes) {
-        node.updateCharge(); // charge with harvested energy, and, discharge with consumed energy
-      }
+        if (ehConfigFile != null)
+            return;
 
-      // Now schedule the next event
-      totalUpdates++;
-      long nextEventTime = startTime + (long)(totalUpdates * getChargeInterval() * 1000000);
-      if (simulation.getSimulationTime() <  nextEventTime) {
-        simulation.scheduleEvent(this, nextEventTime);
-        return;
-      }
-    }
-
-  }
-
-  // --------------------------------------------------------------------------
-  @Override
-  public Collection<Element> getConfigXML() {
-    ArrayList<Element> configXML = new ArrayList<Element>();
-    Element element;
-
-    if (ehConfigFile != null) {
-      element = new Element("eh_config_file");
-      File file = simulation.getGUI().createPortablePath(ehConfigFile);
-      element.setText(file.getPath().replaceAll("\\\\", "/"));
-      element.setAttribute("EXPORT", "copy");
-      configXML.add(element);
-    }
-
-    return configXML;
-  }
-
-  @Override
-  public boolean setConfigXML(Collection<Element> configXML, boolean visAvailable) {
-    for (Element element : configXML) {
-      String name = element.getName();
-
-      if (name.equals("eh_config_file")) {
-        ehConfigFile = simulation.getGUI().restorePortablePath(new File(element.getText()));
+        JFileChooser fileChooser = new JFileChooser();
+        File suggest = new File(GUI.getExternalToolsSetting("DEFAULT_EH_CONFIG",
+                "/home/user/contiki-2.7/tools/cooja/apps/senseh/config/EH.config"));
+        fileChooser.setSelectedFile(suggest);
+        fileChooser.setDialogTitle("Select configuration file for harvesting system");
+        int reply = fileChooser.showOpenDialog(GUI.getTopParentContainer());
+        if (reply == JFileChooser.APPROVE_OPTION) {
+            ehConfigFile = fileChooser.getSelectedFile();
+            GUI.setExternalToolsSetting("DEFAULT_EH_CONFIG", ehConfigFile.getAbsolutePath());
+        }
+        if (ehConfigFile == null) {
+            throw new RuntimeException("No configuration file for harvesting system");
+        }
         init(ehConfigFile.getAbsolutePath());
-      }
     }
 
-    return true;
-  }
-
-  // --------------------------------------------------------------------------
-  public void restartStoredEnergyStatistics() {
-    for (EHNode node : ehNodes) {
-      EnergyStorage storage = node.getEHSystem().getStorage();
-      storage.discharge(storage.getEnergy() * storage.getNumStorages()); // Drain all amount
-    }
-  }
-
-  public void restartHarvestedEnergyStatistics() {
-    for (EHNode node : ehNodes) {
-      EHSystem ehsys = node.getEHSystem();
-      ehsys.setTotalHarvestedEnergy(0);
-    }
-  }
-
-  public void restartConsumedEnergyStatistics() {
-    for (EHNode node : ehNodes) {
-      PowerConsumption consumption = node.getPowerConsumption();
-      consumption.restart(); // Reset total consumption
-    }
-  }
-
-  // --------------------------------------------------------------------------
-  public String radioStatistics() {
-    return radioStatistics(true, true, false);
-  }
-
-  public String radioStatistics(boolean radioHW, boolean radioRXTX, boolean onlyAverage) {
-    StringBuilder sb = new StringBuilder();
-
-    /* Average */
-    PowerConsumption.RadioTimes radioTimesAVG = new PowerConsumption.RadioTimes(0); // omitting tx levels
-    for (EHNode node : ehNodes) {
-      PowerConsumption consumption = node.getPowerConsumption();
-      radioTimesAVG.addMembersWith(consumption.radioTimesTotal);
-    }
-    radioTimesAVG.divMembersWith(ehNodes.length); // Average it!
-    sb.append(PowerConsumption.makeRadioSummaryStatistics(radioHW, radioRXTX,
-        false /* not show duration */, true /* not show idle */, radioTimesAVG, "AVG "));
-
-    /* All nodes */
-    if (!onlyAverage)
-      for (EHNode node : ehNodes) {
-        PowerConsumption consumption = node.getPowerConsumption();
-        sb.append(consumption.radioStatistics());
-      }
-
-    return sb.toString();
-  }
-
-  public String radioTxStatistics() {
-    StringBuilder sb = new StringBuilder();
-    for (EHNode node : ehNodes) {
-      PowerConsumption consumption = node.getPowerConsumption();
-      sb.append(consumption.radioTxStatistics());
-    }
-    return sb.toString();
-  }
-
-  // --------------------------------------------------------------------------
-  public String getStatistics() {
-    return getStatistics("");
-  }
-
-  public String getStatistics(String prefix) {
-    StringBuilder sb = new StringBuilder();
-
-    for (EHNode node : ehNodes) {
-      sb.append(prefix);
-      sb.append("update:us=" + lastUpdateTime + ", ");
-
-      int id  = node.getNodeID();
-      EnergyStorage storage = node.getEHSystem().getStorage();
-      EHSystem ehsys = node.getEHSystem();
-      PowerConsumption consumption = node.getPowerConsumption();
-
-      sb.append("node="   + id + ", ");
-      sb.append("sto:mJ=" + storage.getEnergy() + ", ");
-      sb.append("eh:mJ="  + ehsys.getTotalHarvestedEnergy() + ", ");
-      sb.append(consumption.getSnappedStatistics());
-      sb.append("\n");
+    void init(String configFilePath) {
+        //setTitle("~~~ TITLE ~~~");
+        ehNodes = new EHNode[simulation.getMotesCount()];
+        for (int i = 0; i < simulation.getMotesCount(); i++)
+            ehNodes[i] = new EHNode(i, simulation, configFilePath, this);
+        schedulePeriodicChargeUpdate(); // schedule event to update the charge of all the nodes
     }
 
-    return sb.toString();
-  }
+    private void schedulePeriodicChargeUpdate() {
+        simulation.invokeSimulationThread(new ChargeUpdateTaskScheduler());
+    }
+
+    private double getChargeInterval() { // assume that charge update interval for ALL nodes is equal
+        return ehNodes[0].getEHSystem().getChargeInterval();
+    }
+
+    @Override
+    public void closePlugin() {
+        if (chargeUpdateEvent != null) {
+            chargeUpdateEvent.remove();
+
+            for (int i = 0; i < ehNodes.length; i++) {
+                ehNodes[i].getPowerConsumption().dispose();
+            }
+        }
+    }
+
+    // --------------------------------------------------------------------------
+    private class ChargeUpdateTaskScheduler implements Runnable {
+
+        @Override
+        public void run() {
+            totalUpdates = 1;
+            startTime = simulation.getSimulationTime();
+            lastUpdateTime = 0; // It means never updated before.
+            //logger.debug("periodStart: " + periodStart);
+            chargeUpdateEvent = new ChargeUpdateEvent(0);
+            chargeUpdateEvent.execute(startTime + (long)(getChargeInterval() * 1000000));
+        }
+
+    }
+
+    // --------------------------------------------------------------------------
+    private class ChargeUpdateEvent extends TimeEvent{
+
+        public ChargeUpdateEvent(long t) {
+            super(t, "charge update event");
+        }
+
+        @Override
+        public void execute(long t) {
+            // Detect early events: reschedule for later
+            //System.out.println ("t\t"+t + "\tSimTime\t"+simulation.getSimulationTime());
+            if (simulation.getSimulationTime() < t) {
+                simulation.scheduleEvent(this, startTime + (long)(totalUpdates * getChargeInterval() * 1000000));
+                return;
+            }
+
+            lastUpdateTime = simulation.getSimulationTime();
+            /**
+             * SensEH does NOT continuously count harvested energies and consumed energies.
+             * It updates the charges periodically on every interval.
+             */
+            for (EHNode node : ehNodes) {
+                node.updateCharge();  // charge with harvested energy, and, discharge with consumed energy
+            }
+
+            // Now schedule the next event
+            totalUpdates++;
+            long nextEventTime = startTime + (long)(totalUpdates * getChargeInterval() * 1000000);
+            if (simulation.getSimulationTime() <  nextEventTime) {
+                simulation.scheduleEvent(this, nextEventTime);
+                return;
+            }
+        }
+
+    }
+
+    // --------------------------------------------------------------------------
+    @Override
+    public Collection<Element> getConfigXML() {
+        ArrayList<Element> configXML = new ArrayList<Element>();
+        Element element;
+
+        if (ehConfigFile != null) {
+            element = new Element("eh_config_file");
+            File file = simulation.getGUI().createPortablePath(ehConfigFile);
+            element.setText(file.getPath().replaceAll("\\\\", "/"));
+            element.setAttribute("EXPORT", "copy");
+            configXML.add(element);
+        }
+
+        return configXML;
+    }
+
+    @Override
+    public boolean setConfigXML(Collection<Element> configXML, boolean visAvailable) {
+        for (Element element : configXML) {
+            String name = element.getName();
+
+            if (name.equals("eh_config_file")) {
+                ehConfigFile = simulation.getGUI().restorePortablePath(new File(element.getText()));
+                init(ehConfigFile.getAbsolutePath());
+            }
+        }
+
+        return true;
+    }
+
+    // --------------------------------------------------------------------------
+    public void restartStoredEnergyStatistics() {
+        for (EHNode node : ehNodes) {
+            EnergyStorage storage = node.getEHSystem().getStorage();
+            storage.discharge(storage.getEnergy() * storage.getNumStorages()); // Drain all amount
+        }
+    }
+
+    public void restartHarvestedEnergyStatistics() {
+        for (EHNode node : ehNodes) {
+            EHSystem ehsys = node.getEHSystem();
+            ehsys.setTotalHarvestedEnergy(0);
+        }
+    }
+
+    public void restartConsumedEnergyStatistics() {
+        for (EHNode node : ehNodes) {
+            PowerConsumption consumption = node.getPowerConsumption();
+            consumption.restart(); // Reset total consumption
+        }
+    }
+
+    // --------------------------------------------------------------------------
+    public String radioStatistics() {
+        return radioStatistics(true, true, false);
+    }
+
+    public String radioStatistics(boolean radioHW, boolean radioRXTX, boolean onlyAverage) {
+        StringBuilder sb = new StringBuilder();
+
+        /* Average */
+        PowerConsumption.RadioTimes radioTimesAVG = new PowerConsumption.RadioTimes(0); // omitting tx levels
+        for (EHNode node : ehNodes) {
+            PowerConsumption consumption = node.getPowerConsumption();
+            radioTimesAVG.addMembersWith(consumption.radioTimesTotal);
+        }
+        radioTimesAVG.divMembersWith(ehNodes.length); // Average it!
+        sb.append(PowerConsumption.makeRadioSummaryStatistics(radioHW, radioRXTX,
+                false /* not show duration */, true /* not show idle */, radioTimesAVG, "AVG "));
+
+        /* All nodes */
+        if (!onlyAverage)
+            for (EHNode node : ehNodes) {
+                PowerConsumption consumption = node.getPowerConsumption();
+                sb.append(consumption.radioStatistics());
+            }
+
+        return sb.toString();
+    }
+
+    public String radioTxStatistics() {
+        StringBuilder sb = new StringBuilder();
+        for (EHNode node : ehNodes) {
+            PowerConsumption consumption = node.getPowerConsumption();
+            sb.append(consumption.radioTxStatistics());
+        }
+        return sb.toString();
+    }
+
+    // --------------------------------------------------------------------------
+    public String getStatistics() {
+        return getStatistics("");
+    }
+
+    public String getStatistics(String prefix) {
+        StringBuilder sb = new StringBuilder();
+
+        for (EHNode node : ehNodes) {
+            sb.append(prefix);
+            sb.append("update:us=" + lastUpdateTime + ", ");
+
+            int id  = node.getNodeID();
+            EnergyStorage storage = node.getEHSystem().getStorage();
+            EHSystem ehsys = node.getEHSystem();
+            PowerConsumption consumption = node.getPowerConsumption();
+
+            sb.append("node="   + id + ", ");
+            sb.append("sto:mJ=" + storage.getEnergy() + ", ");
+            sb.append("eh:mJ="  + ehsys.getTotalHarvestedEnergy() + ", ");
+            sb.append(consumption.getSnappedStatistics());
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
 
 }
