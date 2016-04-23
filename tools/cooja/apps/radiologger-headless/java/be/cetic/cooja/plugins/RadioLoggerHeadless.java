d55cabe0b58803b11fa7ae4d81f9187a8b75dbc4
iPAS <ptiwatthanont@gmail.com>
2016-04-23 11:43:57 +0700

    Fixed the clearing function of RadioLogger plugin. Now it works!

--- tools/cooja/apps/radiologger-headless/java/be/cetic/cooja/plugins/RadioLoggerHeadless.java
+++ tools/cooja/apps/radiologger-headless/java/be/cetic/cooja/plugins/RadioLoggerHeadless.java
@@ -33,7 +33,6 @@ package be.cetic.cooja.plugins;
 import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Properties;
 import java.util.Collection;
 import java.util.ArrayList;
 import java.io.File;
@@ -43,7 +42,6 @@ import org.jdom.Element;
 
 import se.sics.cooja.ClassDescription;
 import se.sics.cooja.GUI;
-import se.sics.cooja.Plugin;
 import se.sics.cooja.PluginType;
 import se.sics.cooja.RadioConnection;
 import se.sics.cooja.RadioMedium;
@@ -53,20 +51,26 @@ import se.sics.cooja.VisPlugin;
 import se.sics.cooja.interfaces.Radio;
 //import se.sics.cooja.plugins.analyzers.PacketAnalyser;
 import se.sics.cooja.plugins.analyzers.PcapExporter;
-import se.sics.cooja.util.StringUtils;
-import sun.security.util.Length;
 
 /**
  * Radio Logger which exports a pcap file only.
  * It was designed to support radio logging in COOJA's headless mode.
  * Based on Fredrik Osterlind's RadioLogger.java
  *
+ * Based on RadioLoggerHeadless.java by primary
  * @author Laurent Deru
+ * The work is the stepping stone for keeping separately between destined- and
+ * overheard packets in this source code.
+ *
+ * @author Pasakorn Tiwatthanont
+ * @revised 2015/06 - 2016
  */
 @ClassDescription("Headless radio logger")
 @PluginType(PluginType.SIM_PLUGIN)
 public class RadioLoggerHeadless extends VisPlugin {
     private static final long serialVersionUID = -6927091711697081353L;
+
+    private static final String pcapExt = ".pcap";
 
     private final Simulation simulation;
     private RadioMedium     radioMedium;
@@ -75,6 +79,7 @@ public class RadioLoggerHeadless extends VisPlugin {
     private PcapExporter    pcapSendingExporter;
     private PcapExporter [] pcapReceivingExporter;
     private int motesCount;
+
 
     /** ***********************************************************************
      * Constructor
@@ -95,25 +100,32 @@ public class RadioLoggerHeadless extends VisPlugin {
             pcapReceivingExporter = new PcapExporter[motesCount];
             for (int i = 0; i < motesCount; i++)
                 pcapReceivingExporter[i] = new PcapExporter();
-
         } catch (IOException e) {
+            System.err.println("RadioLogger opens PCAP files error!");
             e.printStackTrace();
         }
 
-        radioMedium.addRadioMediumObserver(radioMediumObserver = new Observer() {
+        startRadioObservation();
+    }
+
+    /** ***********************************************************************
+     * Start the observer
+     */
+    public void startRadioObservation() {
+
+        radioMediumObserver = new Observer() {
             @Override
             public void update(Observable obs, Object obj) {
                 RadioConnection conn = radioMedium.getLastConnection();
-                if (conn == null) {
+                if (conn == null)
                     return;
-                }
+
                 RadioPacket radioTxPacket = conn.getSource().getLastPacketTransmitted();
 
                 /**
-                 * From receiver's view
-                 * [iPAS]: xxx  */
+                 * [iPAS]: From receiver's view  */
                 //for (Radio radioRx : conn.getAllDestinations()) {  // All destination radios including interfered ones
-                for (Radio radioRx : conn.getDestinations()) {  // All non-interfered radios                  
+                for (Radio radioRx : conn.getDestinations()) {  // All non-interfered radios
                     //RadioPacket radioRxPacket = radioRx.getLastPacketReceived();  // It is always null !?
                     try {
                         int i = radioRx.getMote().getID() - 1;
@@ -125,8 +137,7 @@ public class RadioLoggerHeadless extends VisPlugin {
                 }
 
                 /**
-                 * From sender's view 
-                 * [iPAS]: xxx  */
+                 * [iPAS]: From sender's view  */
                 try {
                     pcapSendingExporter.exportPacketData( radioTxPacket.getPacketData() );
                 } catch (IOException e) {
@@ -134,7 +145,17 @@ public class RadioLoggerHeadless extends VisPlugin {
                     e.printStackTrace();
                 }
             }
-        });
+        };
+
+        radioMedium.addRadioMediumObserver(radioMediumObserver);  // Add to the medium
+    }
+
+    /** ***********************************************************************
+     * Stop the observer
+     */
+    public void stopRadioObservation() {
+        if (radioMediumObserver != null)
+            radioMedium.deleteRadioMediumObserver(radioMediumObserver);
     }
 
     /** ***********************************************************************
@@ -142,28 +163,63 @@ public class RadioLoggerHeadless extends VisPlugin {
      */
     @Override
     public void closePlugin() {
-        if (radioMediumObserver != null)
-            radioMedium.deleteRadioMediumObserver(radioMediumObserver);
+        stopRadioObservation();
     }
-    
+
+    /** ***********************************************************************
+     * Create PCAP output files
+     */
+    private void createPcapFiles(String fpath) throws IOException {
+        pcapFile = simulation.getGUI().restorePortablePath(new File(fpath + pcapExt));
+        pcapSendingExporter.openPcap(pcapFile);
+
+        for (int i = 0; i < motesCount; i++)
+            pcapReceivingExporter[i].openPcap(
+                    simulation.getGUI().restorePortablePath(
+                            new File(fpath + "_" + (i + 1) + pcapExt)));
+    }
+
     /** ***********************************************************************
      * Restart statistical values
      */
-    private void setZero(File f) {
+
+    /*
+     * XXX: This method is not work, still don't know why.
+     * Guess that multiple accessing to the file is a cause.
+     *
+    private void setFileZero(File f) {
         try {
             System.err.println("Clear PCAP file " + f.getName());
-            
+
             RandomAccessFile raf = new RandomAccessFile(f, "rw");
             raf.setLength(0);
         } catch (Exception e) {
             System.err.println("Exception on clearing" + e);
-        }        
+        }
     }
-    
+
     public void restartStatistics() {
-        setZero(pcapSendingExporter.pcapFile);        
-        for (int i = 0; i < motesCount; i++)
-            setZero(pcapReceivingExporter[i].pcapFile);
+        try {  // Make it clean & recreate the headers
+            setFileZero(pcapSendingExporter.pcapFile);
+            pcapSendingExporter.exportHeader();
+            for (int i = 0; i < motesCount; i++) {
+                setFileZero(pcapReceivingExporter[i].pcapFile);
+                pcapReceivingExporter[i].exportHeader();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
+
+    public void restartStatistics() {
+        try {  // Make it clean & recreate the headers
+            pcapSendingExporter.reOpenPcap();
+            for (int i = 0; i < motesCount; i++) {
+                pcapReceivingExporter[i].reOpenPcap();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /** ***********************************************************************
@@ -177,22 +233,14 @@ public class RadioLoggerHeadless extends VisPlugin {
 
                 /**
                  * Read file-path configuration  */
-                final String fext = ".pcap";
-                String fname = element.getText();
-                if (fname.lastIndexOf(fext) >= 0)
-                    fname = fname.substring(0, fname.lastIndexOf(fext));
+                String fpath = element.getText();
+                if (fpath.lastIndexOf(pcapExt) >= 0)
+                    fpath = fpath.substring(0, fpath.lastIndexOf(pcapExt));
 
                 /**
                  * Setup the exporter  */
-                pcapFile = simulation.getGUI().restorePortablePath(new File(fname + fext));
                 try {
-                    pcapSendingExporter.openPcap(pcapFile);
-
-                    for (int i = 0; i < motesCount; i++)
-                        pcapReceivingExporter[i].openPcap(
-                                simulation.getGUI().restorePortablePath(
-                                        new File(fname + "_" + (i+1) + fext)));
-
+                    createPcapFiles(fpath);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- tools/cooja/java/se/sics/cooja/plugins/analyzers/PcapExporter.java
+++ tools/cooja/java/se/sics/cooja/plugins/analyzers/PcapExporter.java
@@ -5,25 +5,63 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+/**
+ * Adopted from PcapExporter.java of Cooja simulator which
+ *  is created by
+ * @author Fredrik Osterlind.
+ *
+ * Revised from the original version by iPAS so that
+ * it supports NEW RadioLoggerHeadless version.
+ *
+ * @author Pasakorn Tiwatthanont
+ */
 public class PcapExporter {
 
     DataOutputStream out;
     public File pcapFile;
 
-
     public PcapExporter() throws IOException {
     }
 
+    /**
+     * Open & close PCAP data file.
+     * @throws IOException
+     */
     public void openPcap() throws IOException {
         pcapFile = new File("radiolog-" + System.currentTimeMillis() + ".pcap");
         openPcap(pcapFile);
     }
+
+    public void openPcap(DataOutputStream os) throws IOException {
+        exportHeader(os);
+        System.out.println("Opened PCAP file!");
+    }
+
     public void openPcap(File file) throws IOException {
         pcapFile = file;
         out = new DataOutputStream(new FileOutputStream(file));
         openPcap(out);
     }
-    public void openPcap(DataOutputStream out) throws IOException {
+
+    public void closePcap() throws IOException {
+        out.close();
+    }
+
+    public void reOpenPcap() throws IOException {
+        closePcap();
+        openPcap(pcapFile);
+    }
+
+    /**
+     * Export header and packet data.
+     * @throws IOException
+     */
+    public void exportHeader() throws IOException {
+        exportHeader(out);
+        System.out.println("Exported header!");
+    }
+
+    public void exportHeader(DataOutputStream out) throws IOException {
         /* pcap header */
         out.writeInt(0xa1b2c3d4);
         out.writeShort(0x0002);
@@ -33,10 +71,6 @@ public class PcapExporter {
         out.writeInt(4096);
         out.writeInt(195); /* 195 for LINKTYPE_IEEE802_15_4 */
         out.flush();
-        System.out.println("Opened PCAP file!");
-    }
-    public void closePcap() throws IOException {
-        out.close();
     }
 
     public void exportPacketData(byte[] data) throws IOException {
@@ -56,7 +90,5 @@ public class PcapExporter {
             e.printStackTrace();
         }
     }
-
-
 
 }
